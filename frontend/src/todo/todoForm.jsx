613f491f444f3f85e5a9d292e013d4ad0d565869
Hugo Amaral <hugowm0@gmail.com>
2019-09-03 16:24:49 -0300

    finalização do todo-app

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import Grid from '../template/grid';
+import IconButton from '../template/iconButton';
 
-export default props => (
-    <div>
-        <h1>form</h1>
-    </div>
-)
+export default props => {
+    const keyHandler = e => {
+        if(e.key === "Enter"){
+            e.shiftKey ? props.handleSearch() : props.handleAdd();
+        } else if(e.key === 'Escape'){
+            props.handleClear();
+        }
+    }
+
+    return (
+        <div role='form' className='todoForm'>
+            <Grid cols='12 9 10'>
+                <input id="description" onKeyUp={keyHandler} value={props.description} onChange={props.handleChange} className='form-control' placeholder='adicione uma tarefa' />
+            </Grid>
+            <Grid cols='12 3 2'>
+                <IconButton style='primary' icon='plus' onClick={props.handleAdd} />
+                <IconButton style='info' icon='search' onClick={props.handleSearch} />
+                <IconButton style='default' icon='close' onClick={props.handleClear} />
+            </Grid>
+        </div>
+    )
+}
